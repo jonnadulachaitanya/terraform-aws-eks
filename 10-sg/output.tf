3213262ea4c07607f85be55717823cadf42c66b7
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2026-04-04 12:18:14 +0530

    file committed

--- 10-sg/output.tf
+++ 10-sg/output.tf
@@ -1,18 +1,15 @@
-output "sg_id" {
-  value = module.mysql_sg.sg_id
-}
 output "ingress_alb_sg_id" {
-  value = aws_security_group.main.id
+  value = module.ingress_alb_sg.id
 }
 output "node_sg_id" {
   value = aws_security_group.main.id
 }
 output "eks_control_plane_sg_id" {
-  value = aws_security_group.main.id
+  value = module.eks_control_plane_sg.id
 }
 output "bastion_sg_id" {
-  value = aws_security_group.main.id
+  value = module.bastion_sg.id
 }
 output "mysql_sg_id" {
-  value = aws_security_group.main.id
+  value = module.mysql_sg.id
 }
