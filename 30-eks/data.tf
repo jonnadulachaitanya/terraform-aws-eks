e321183359404abf0dc9f0bb989a4294a91f2fec
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2026-04-06 15:37:20 +0530

    file committed

--- 30-eks/data.tf
+++ 30-eks/data.tf
@@ -1,8 +1,8 @@
 data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc_id"
 }
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+data "aws_ssm_parameter" "private_subnet_id" {
+  name = "/${var.project_name}/${var.environment}/private_subnet_id"
 }
 
 data "aws_ssm_parameter" "eks_control_plane_sg_id" {
