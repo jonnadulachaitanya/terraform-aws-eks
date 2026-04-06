0459cf1ea63072a6924b0018fa7f04e54c6a9659
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2026-04-06 15:34:07 +0530

    file committed

--- 30-eks/main.tf
+++ 30-eks/main.tf
@@ -2,7 +2,7 @@ resource "aws_key_pair" "eks" {
   key_name = "eks"
   # you can paste the public key directly like this
   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = file("~/.ssh/eks.pub")
+  public_key = file("F:/devops/keys/eks.pub")
   # ~ means windows home directory
 }
 
